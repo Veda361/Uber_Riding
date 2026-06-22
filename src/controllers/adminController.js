edf92f5a81780f98c453c496b267582b5951ef16
Dev Sahu <devranjeetq@gmail.com>
2026-06-22 15:27:14 +0530

    stats of adimin, drivers, payments get fetched by admin controller

---
+++ src/controllers/adminController.js
@@ -0,0 +1,146 @@
+const User = require("../model/User");
+
+const Driver = require("../model/Driver");
+
+const Ride = require("../model/Ride");
+
+const Payment = require("../model/Payment");
+
+
+const getAdminStats = async(req, res) => {
+
+    try{
+        const totalUsers = await Users.countDocuments();
+
+        const totalDrivers = await Driver.countDocuments();
+
+        const activeDrivers = await Driver.countDocuments({
+            isAvailable: true,
+    });
+
+        const totalRides = await Ride.countDocuments();
+
+        const completedRides = await Ride.countDocuments({
+            status: "Completed",
+        });
+
+        const cancelledRides = await Ride.countDocuments({
+            status: "Cancelled",
+        });
+
+        const payments = await Payment.find({
+            status: "paid",
+        });
+
+        const totalRevenues = 
+        payments.reduce(
+            (sum, payment) => sum+payment.amount, 0
+        );
+
+        res.json({
+            success: true,
+            status: 
+            totalUsers,
+            totalDrivers,
+            activeDrivers,
+            totalRides,
+            completedRides,
+            cancelledRides,
+            totalRevenues,
+        });
+        }catch(error){
+            res.status(500).json({
+                message: error.message,
+            });
+        }
+
+        
+};
+
+const getAllUsers = async(req, res) => {
+    try{
+        const users = await User.find().sort({
+            createAt: -1,
+        });
+
+        res.json({
+            success: true,
+            count: users.length,
+            users,
+        });
+    }catch(error){
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+
+const getAllDrivers = async(req, res) => {
+    try{
+        const drivers = await Driver.find()
+        .populate("userId")
+        .sort({
+            createdAt: -1,
+        });
+
+        res.json({
+            success: true,
+            count: drivers.length,
+            drivers,
+        });
+    }catch(error){
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+
+const getAllRides = async(req, res) => {
+    try{
+        const rides = (await Ride.find().populate("riderId").populate("driverId")).sort({
+            createdAt: -1,
+        });
+        res.json({
+            success: true,
+            count: Ride.length,
+            rides,
+        });
+
+    }catch(error){
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+const getAllPayments = async(req, res) => {
+    try{
+        const payments = await Payment.find()
+                                .populate("userId")
+                                .populate("rideId")
+                                .sort({createdAt: -1});
+
+            res.json({
+                success: true,
+                count: payments.length,
+                payments,
+            });
+
+    }catch(error){
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+
+};
+
+
+module.exports = {
+    getAdminStats,
+    getAllUsers,
+    getAllDrivers,
+    getAllRides,
+    getAllPayments
+}
